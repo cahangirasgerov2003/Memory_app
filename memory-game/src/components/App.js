c15462e14341857a47bf1a1dd629a911267b6b1f
CahangirAsgerov <esgerovcahangir12@gmail.com>
2023-07-16 16:06:05 +0400

    This commit message will make the memory game playable

--- memory-game/src/components/App.js
+++ memory-game/src/components/App.js
@@ -4,52 +4,149 @@ import CardsList from "./CardsList";
 let informationCards = [
   {
     url: "1.jpeg",
+    selected: false,
   },
   {
     url: "2.jpeg",
+    selected: false,
   },
   {
     url: "3.jpeg",
+    selected: false,
   },
   {
     url: "4.jpeg",
+    selected: false,
   },
   {
     url: "5.jpeg",
+    selected: false,
   },
   {
     url: "6.jpeg",
+    selected: false,
   },
   {
     url: "7.jpeg",
+    selected: false,
   },
   {
     url: "8.jpeg",
+    selected: false,
   },
 ];
 
-informationCards = [...informationCards, ...informationCards];
+let score = 0;
+let trueSelected = 0;
 
 const App = () => {
   const [cards, setCards] = useState([]);
+  const [firstClickCard, setfirstClickCard] = useState(null);
+  const [secondClickCard, setsecondClickCard] = useState(null);
+  const [controlClickCard, setcontrolClickCard] = useState(true);
+  const [record, setRecord] = useState("No record");
+  const sortingCards = () => {
+    setCards(
+      [...informationCards, ...informationCards]
+        .sort(() => Math.random() - 0.5)
+        .map((card) => {
+          return { ...card, key: Math.random() };
+        })
+    );
+
+    score = 0;
+    trueSelected = 0;
+
+    setfirstClickCard(null);
+    setsecondClickCard(null);
+  };
+
+  const handleSelected = (card) => {
+    firstClickCard
+      ? card.key !== firstClickCard.key
+        ? setsecondClickCard(card)
+        : setsecondClickCard(null)
+      : setfirstClickCard(card);
+  };
+
+  const resetState = () => {
+    setfirstClickCard(null);
+    setsecondClickCard(null);
+    setcontrolClickCard(true);
+  };
+
+  const changeRecord = (score) => {
+    setTimeout(() => {
+      if (typeof record === "string" || score < record) {
+        setRecord(score);
+      }
+    }, 1000);
+  };
 
   useEffect(() => {
-    for (let i = informationCards.length - 1; i >= 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [informationCards[i], informationCards[j]] = [
-        informationCards[j],
-        informationCards[i],
-      ];
-      informationCards[i].key = Math.random();
+    const myRecord = window.localStorage.getItem("record");
+
+    if (myRecord) {
+      setRecord(JSON.parse(myRecord));
     }
-    setCards(informationCards);
+    sortingCards();
   }, []);
 
+  useEffect(() => {
+    if (firstClickCard && secondClickCard) {
+      setcontrolClickCard(false);
+
+      score = score + 1;
+
+      if (firstClickCard.url === secondClickCard.url) {
+        trueSelected = trueSelected + 1;
+
+        if (trueSelected === 8) {
+          changeRecord(score);
+        }
+
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.url === firstClickCard.url) {
+              return {
+                ...card,
+                selected: true,
+              };
+            }
+            return card;
+          });
+        });
+
+        setTimeout(() => {
+          resetState();
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          resetState();
+        }, 1000);
+      }
+    }
+  }, [firstClickCard, secondClickCard]);
+
+  useEffect(() => {
+    window.localStorage.setItem("record", JSON.stringify(record));
+  }, [record]);
+
   return (
-    <div className="mt-4">
+    <div className="containerApp">
       <h1>Memory Game</h1>
-      <h2>Score: 0</h2>
-      <CardsList cards={cards} />
+      <h2 className="h5 mt-2">Your Record : {record}</h2>
+      <button onClick={sortingCards} className="btn btn-success mt-2">
+        Play Game
+      </button>
+      <CardsList
+        cards={cards}
+        handleSelected={handleSelected}
+        controlClickCard={controlClickCard}
+        firstClickCard={firstClickCard}
+        secondClickCard={secondClickCard}
+        score={score}
+      />
     </div>
   );
 };
--- memory-game/src/components/CardsList.js
+++ memory-game/src/components/CardsList.js
@@ -1,14 +1,45 @@
 import React from "react";
 
-const CardsList = ({ cards }) => {
-  return cards.map((card) => {
-    return (
-      <div className="card" key={card.key}>
-        <img src={`./image/${card.url}`} alt=""></img>
-        <img src="./image/cover.jpeg" alt=""></img>
-      </div>
-    );
-  });
+const CardsList = ({
+  cards,
+  handleSelected,
+  controlClickCard,
+  firstClickCard,
+  secondClickCard,
+}) => {
+  return (
+    <div className="cardsContainer">
+      {cards.map((card) => {
+        return (
+          <div className="imagesContainer" key={card.key}>
+            <div
+              className={
+                firstClickCard?.key === card.key ||
+                secondClickCard?.key === card.key ||
+                card.selected
+                  ? "rotated"
+                  : ""
+              }
+            >
+              <img
+                className={`backImage`}
+                src={`./image/${card.url}`}
+                alt=""
+              ></img>
+              <img
+                className={`frondImage`}
+                src="./image/cover.jpeg"
+                alt=""
+                onClick={() => {
+                  return controlClickCard ? handleSelected(card) : null;
+                }}
+              ></img>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CardsList;
